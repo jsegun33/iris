cc58580212313d0a6dacc8d35e7e121613488f87
unknown <you@example.com>
2020-07-02 14:02:53 +0800

    Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUsersTable extends Migration
 {
@@ -43,7 +44,7 @@ class CreateUsersTable extends Migration
             'userRole'      => 'Super Admin',
             'username'      => 'admin',
             'email'         => 'admin@iristech.ph',
-            'password'      => bcrypt::make('12345678'),
+            'password'      => Hash::make('12345678'),
             'ApprovedLimit' => 1000000,
             'AccountNo'     => $AccountNo,
             'created_at'    => now(),
